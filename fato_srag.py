9cb9531d79a7378d75d7ad501defd76cab817fec
FlorSousa <jhonatas.flor@dcx.ufpb.br>
2023-06-27 07:31:47 -0300

    criando dimensões e relacionamentos

--- fato_srag.py
+++ fato_srag.py
@@ -1,1 +1,27 @@
-#criação da tabela fato
+import os
+import time
+import pandas as pd
+from dotenv import load_dotenv
+from models.srag_models import *
+from sqlalchemy import inspect, create_engine
+from urllib.parse import quote_plus as urlquote
+load_dotenv()
+inicio = time.time()
+engine = create_engine(f"postgresql+psycopg2://{os.getenv('USER')}:{urlquote(os.getenv('PASSWORD'))}@{os.getenv('HOST')}:5432/{os.getenv('DB_NAME')}",echo=False)
+
+def cria_dimensao():
+    try:
+        print("Iniciando conexão")
+        print("Conexão feita")
+        print("Removendo as dimensões existentes")
+        Base.metadata.drop_all(engine)
+        print("Criando as dimensões")
+        Base.metadata.create_all(engine) 
+        print("Dimensões criadas")
+    except Exception as e:
+        print(F"Erro:{e}")
+
+cria_dimensao()
+
+fim = time.time()
+print(f"Tempo total: {(fim-inicio)/60}")
---
+++ models/srag_models.py
@@ -0,0 +1,52 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class DimensaoData(Base):
+    __tablename__ = 'dimensao_data_srag'
+    
+    id = Column(Integer, primary_key=True)
+    data = Column(String(8), nullable=False, default="1/2022")
+    mes = Column(Integer, default=1)
+    ano = Column(String, default=2022)
+
+class DimensaoSexoSrag(Base):
+    __tablename__ = 'dimensao_sexo_srag'
+
+    id = Column(Integer, primary_key=True)
+    sexo = Column(String(2), nullable=False, default='1')
+    sexo_num = Column(Integer, nullable=False, default=1)
+
+class DimensaoEstado(Base):
+    __tablename__ = 'dimensao_estado_srag'
+    id = Column(Integer, primary_key=True)
+    nome_estado = Column(String(40), nullable=False, default='1')
+    uf = Column(String(2), nullable=False, default='PB')
+    regiao = Column(String(40), nullable=False, default='regiao')
+    codigo_ibge = Column(Integer, nullable=False, default=0)
+
+class DimensaoOcupacaoSrag(Base):
+    __tablename__ = 'dimensao_ocupacao_srag'
+    
+    id = Column(Integer, primary_key=True)
+    cbo_codigo = Column(String(255), nullable=False, default='1')
+    nome_profissao = Column(String(255), nullable=False, default='profissao')
+    profissional_saude = Column(Boolean, nullable=False, default=True)
+
+class FatoSrag(Base):
+    __tablename__ = 'fato_srag'
+    
+    id = Column(Integer, primary_key=True)
+    id_cbo_fato = Column(Integer, ForeignKey(DimensaoOcupacaoSrag.id), nullable=False)
+    id_ano_fato = Column(Integer, ForeignKey(DimensaoData.id), nullable=True)
+    id_mes_fato = Column(Integer, ForeignKey(DimensaoData.id), nullable=True)
+    id_sexo_fato = Column(Integer, ForeignKey(DimensaoSexoSrag.id), nullable=False)
+    id_estado_fato = Column(Integer, ForeignKey(DimensaoEstado.id), nullable=False)
+
+    cbo_fato = relationship(DimensaoOcupacaoSrag, backref="fatos")
+    ano_fato = relationship(DimensaoData, foreign_keys=[id_ano_fato], backref="fatos_ano")
+    mes_fato = relationship(DimensaoData, foreign_keys=[id_mes_fato], backref="fatos_mes")
+    sexo_fato = relationship(DimensaoSexoSrag, backref="fatos")
+    estado_fato = relationship(DimensaoEstado, backref="fatos")
